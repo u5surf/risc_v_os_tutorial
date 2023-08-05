c06d6af7168c9599c141c7475c738ee5a557d619
yugo-horie <u5.horie@gmail.com>
2023-08-05 23:16:38 +0900

    Add some libc

--- kernel.c
+++ kernel.c
@@ -27,6 +27,8 @@ void putchar(char ch) {
 }
 
 void kernel_main(void) {
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
+    
     printf("\n\nHello %s\n", "World!");
     printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
