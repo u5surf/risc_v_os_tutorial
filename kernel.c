735b1e820b4817b4edd79592d83f12ed9bd92a58
yugo-horie <u5.horie@gmail.com>
2023-08-08 22:26:38 +0900

    Add scheduler

--- kernel.c
+++ kernel.c
@@ -30,7 +30,7 @@ __attribute__((naked))
 __attribute__((aligned(4)))
 void kernel_entry(void) {
     __asm__ __volatile__(
-        "csrw sscratch, sp\n"
+        "csrrw sp, sscratch, sp\n"
         "addi sp, sp, -4 * 31\n"
         "sw ra, 4 * 0(sp)\n"
         "sw gp, 4 * 1(sp)\n"
@@ -65,6 +65,9 @@ void kernel_entry(void) {
 
         "csrr a0, sscratch\n"
         "sw a0, 4 * 30(sp)\n"
+
+        "addi a0, sp, 4 *31\n"
+        "csrw sscratch, a0\n"
 
         "mv a0, sp\n"
         "call handle_trap\n"
@@ -198,6 +201,34 @@ struct process *create_process(uint32_t pc) {
     return proc;
 }
 
+struct process *current_proc;
+struct process *idle_proc;
+
+void yield(void) {
+    struct process *next = idle_proc;
+    for (int i = 0; i < PROCS_MAX; i++) {
+        struct process *proc = &procs[(current_proc->pid + i) % PROCS_MAX];
+        if (proc->state == PROC_RUNNABLE && proc->pid > 0) {
+            next = proc;
+            break;
+        }
+    }
+
+    if (next == current_proc)
+        return;
+
+    __asm__ __volatile__(
+        "csrw sscratch, %[sscratch]\n"
+        :
+        : [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
+    );
+
+    struct process *prev = current_proc;
+    current_proc = next;
+    switch_context(&prev->sp, &next->sp);
+}
+
+
 struct process *proc_a;
 struct process *proc_b;
 
@@ -205,7 +236,7 @@ void proc_a_entry(void) {
     printf("starting process A\n");
     while (1) {
         putchar('A');
-        switch_context(&proc_a->sp, &proc_b->sp);
+        yield();
 
         for(int i=0; i < 30000000; i++)
             __asm__ __volatile__("nop");
@@ -216,7 +247,7 @@ void proc_b_entry(void) {
     printf("starting process B\n");
     while (1) {
         putchar('B');
-        switch_context(&proc_b->sp, &proc_a->sp);
+        yield();
 
         for(int i=0; i < 30000000; i++)
             __asm__ __volatile__("nop");
@@ -226,12 +257,18 @@ void proc_b_entry(void) {
 void kernel_main(void) {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
+    printf("\n\n");
+
     WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    idle_proc = create_process((uint32_t) NULL);
+    idle_proc->pid = -1;
+    current_proc = idle_proc;
 
     proc_a = create_process((uint32_t) proc_a_entry);
     proc_b = create_process((uint32_t) proc_b_entry);
 
-    proc_a_entry();
+    yield();
     PANIC("unreachable here");
 
     //printf("\n\nHello %s\n", "World!");
